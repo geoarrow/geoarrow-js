cbfac73d1b8ff022fec73f2b062f26c77f3f9cc9
Kyle Barron <kyle@developmentseed.org>
2023-11-30 15:47:45 -0500

    bump to 0.2.0 (#19)
    * Update readme
    
    * update changelog

--- src/algorithm/index.ts
+++ src/algorithm/index.ts
@@ -1,5 +1,6 @@
 export { area, signedArea } from "./area.js";
 export { earcut } from "./earcut.js";
+export { getPolygonExterior, getMultiPolygonExterior } from "./exterior.js";
 export { mapCoords } from "./coords.js";
 export { reproject } from "./proj.js";
 export { totalBounds } from "./total-bounds.js";
