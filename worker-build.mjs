60aefbec39901fc88af73816511f6d73e051ee47
Kyle Barron <kyle@developmentseed.org>
2025-10-17 17:18:35 -0400

    feat: Worker build improvements

--- worker-build.mjs
+++ worker-build.mjs
@@ -1,20 +1,37 @@
 // worker-build.mjs
 import esbuild from "esbuild";
 
-esbuild.build({
-  entryPoints: ["./src/worker-bundle/earcut.ts"],
-  outfile: "dist/earcut-worker.js",
-  bundle: true,
-  minify: false,
-  target: ["esnext"],
-  format: "esm",
-});
+// dist/earcut.worker.js was added to be easier for esbuild to consume, as it
+// allows loading as text via:
+// ```
+// loader: {
+//   ".worker.js": "text",
+//   ".worker.min.js": "text",
+// },
+// ```
+// We keep dist/earcut-worker.js for backwards compatibility.
+for (const outfile of ["dist/earcut-worker.js", "dist/earcut.worker.js"]) {
+  esbuild.build({
+    entryPoints: ["./src/worker-bundle/earcut.ts"],
+    outfile,
+    bundle: true,
+    minify: false,
+    target: ["esnext"],
+    format: "esm",
+  });
+}
 
-esbuild.build({
-  entryPoints: ["./src/worker-bundle/earcut.ts"],
-  outfile: "dist/earcut-worker.min.js",
-  bundle: true,
-  minify: true,
-  target: ["esnext"],
-  format: "esm",
-});
+// Likewise, earcut-worker.min.js is for backwards compatibility.
+for (const outfile of [
+  "dist/earcut-worker.min.js",
+  "dist/earcut.worker.min.js",
+]) {
+  esbuild.build({
+    entryPoints: ["./src/worker-bundle/earcut.ts"],
+    outfile,
+    bundle: true,
+    minify: true,
+    target: ["esnext"],
+    format: "esm",
+  });
+}
