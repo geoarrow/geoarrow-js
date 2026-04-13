5c47e0626d170eb55b3a4480840979eff5730476
Kyle Barron <kyle@developmentseed.org>
2026-04-13 11:22:08 -0400

    fix narrowing test imports

--- tests/geo-interface/narrowing.test.ts
+++ tests/geo-interface/narrowing.test.ts
@@ -1,83 +1,83 @@
 import { describe, expectTypeOf, it } from "vitest";
 import type {
-  Dimension,
-  GeometryTrait,
-  LineStringTrait,
-  MultiLineStringTrait,
-  MultiPointTrait,
-  MultiPolygonTrait,
-  PointTrait,
-  PolygonTrait,
-  GeometryCollectionTrait,
+	Dimension,
+	GeometryCollectionInterface,
+	GeometryInterface,
+	LineStringInterface,
+	MultiLineStringInterface,
+	MultiPointInterface,
+	MultiPolygonInterface,
+	PointInterface,
+	PolygonInterface,
 } from "../../src/geo-interface/index.js";
 import { sizeOf } from "../../src/geo-interface/index.js";
 
-describe("GeometryTrait narrowing", () => {
-  it("narrows to PointTrait on 'Point'", () => {
-    const g = {} as GeometryTrait;
-    if (g.geometryType === "Point") {
-      expectTypeOf(g).toMatchTypeOf<PointTrait>();
-    }
-  });
+describe("GeometryInterface narrowing", () => {
+	it("narrows to PointInterface on 'Point'", () => {
+		const g = {} as GeometryInterface;
+		if (g.geometryType === "Point") {
+			expectTypeOf(g).toMatchTypeOf<PointInterface>();
+		}
+	});
 
-  it("narrows to LineStringTrait on 'LineString'", () => {
-    const g = {} as GeometryTrait;
-    if (g.geometryType === "LineString") {
-      expectTypeOf(g).toMatchTypeOf<LineStringTrait>();
-    }
-  });
+	it("narrows to LineStringInterface on 'LineString'", () => {
+		const g = {} as GeometryInterface;
+		if (g.geometryType === "LineString") {
+			expectTypeOf(g).toMatchTypeOf<LineStringInterface>();
+		}
+	});
 
-  it("narrows to PolygonTrait on 'Polygon'", () => {
-    const g = {} as GeometryTrait;
-    if (g.geometryType === "Polygon") {
-      expectTypeOf(g).toMatchTypeOf<PolygonTrait>();
-    }
-  });
+	it("narrows to PolygonInterface on 'Polygon'", () => {
+		const g = {} as GeometryInterface;
+		if (g.geometryType === "Polygon") {
+			expectTypeOf(g).toMatchTypeOf<PolygonInterface>();
+		}
+	});
 
-  it("narrows to MultiPointTrait on 'MultiPoint'", () => {
-    const g = {} as GeometryTrait;
-    if (g.geometryType === "MultiPoint") {
-      expectTypeOf(g).toMatchTypeOf<MultiPointTrait>();
-    }
-  });
+	it("narrows to MultiPointInterface on 'MultiPoint'", () => {
+		const g = {} as GeometryInterface;
+		if (g.geometryType === "MultiPoint") {
+			expectTypeOf(g).toMatchTypeOf<MultiPointInterface>();
+		}
+	});
 
-  it("narrows to MultiLineStringTrait on 'MultiLineString'", () => {
-    const g = {} as GeometryTrait;
-    if (g.geometryType === "MultiLineString") {
-      expectTypeOf(g).toMatchTypeOf<MultiLineStringTrait>();
-    }
-  });
+	it("narrows to MultiLineStringInterface on 'MultiLineString'", () => {
+		const g = {} as GeometryInterface;
+		if (g.geometryType === "MultiLineString") {
+			expectTypeOf(g).toMatchTypeOf<MultiLineStringInterface>();
+		}
+	});
 
-  it("narrows to MultiPolygonTrait on 'MultiPolygon'", () => {
-    const g = {} as GeometryTrait;
-    if (g.geometryType === "MultiPolygon") {
-      expectTypeOf(g).toMatchTypeOf<MultiPolygonTrait>();
-    }
-  });
+	it("narrows to MultiPolygonInterface on 'MultiPolygon'", () => {
+		const g = {} as GeometryInterface;
+		if (g.geometryType === "MultiPolygon") {
+			expectTypeOf(g).toMatchTypeOf<MultiPolygonInterface>();
+		}
+	});
 
-  it("narrows to GeometryCollectionTrait on 'GeometryCollection'", () => {
-    const g = {} as GeometryTrait;
-    if (g.geometryType === "GeometryCollection") {
-      expectTypeOf(g).toMatchTypeOf<GeometryCollectionTrait>();
-    }
-  });
+	it("narrows to GeometryCollectionInterface on 'GeometryCollection'", () => {
+		const g = {} as GeometryInterface;
+		if (g.geometryType === "GeometryCollection") {
+			expectTypeOf(g).toMatchTypeOf<GeometryCollectionInterface>();
+		}
+	});
 });
 
-describe("GeometryTrait union membership", () => {
-  it("each concrete trait is assignable to GeometryTrait", () => {
-    expectTypeOf<PointTrait>().toMatchTypeOf<GeometryTrait>();
-    expectTypeOf<LineStringTrait>().toMatchTypeOf<GeometryTrait>();
-    expectTypeOf<PolygonTrait>().toMatchTypeOf<GeometryTrait>();
-    expectTypeOf<MultiPointTrait>().toMatchTypeOf<GeometryTrait>();
-    expectTypeOf<MultiLineStringTrait>().toMatchTypeOf<GeometryTrait>();
-    expectTypeOf<MultiPolygonTrait>().toMatchTypeOf<GeometryTrait>();
-    expectTypeOf<GeometryCollectionTrait>().toMatchTypeOf<GeometryTrait>();
-  });
+describe("GeometryInterface union membership", () => {
+	it("each concrete Interface is assignable to GeometryInterface", () => {
+		expectTypeOf<PointInterface>().toMatchTypeOf<GeometryInterface>();
+		expectTypeOf<LineStringInterface>().toMatchTypeOf<GeometryInterface>();
+		expectTypeOf<PolygonInterface>().toMatchTypeOf<GeometryInterface>();
+		expectTypeOf<MultiPointInterface>().toMatchTypeOf<GeometryInterface>();
+		expectTypeOf<MultiLineStringInterface>().toMatchTypeOf<GeometryInterface>();
+		expectTypeOf<MultiPolygonInterface>().toMatchTypeOf<GeometryInterface>();
+		expectTypeOf<GeometryCollectionInterface>().toMatchTypeOf<GeometryInterface>();
+	});
 });
 
 describe("Dimension and sizeOf type contract", () => {
-  it("sizeOf accepts a Dimension literal", () => {
-    expectTypeOf(sizeOf).parameter(0).toEqualTypeOf<Dimension>();
-    expectTypeOf(sizeOf).returns.toBeNumber();
-  });
+	it("sizeOf accepts a Dimension literal", () => {
+		expectTypeOf(sizeOf).parameter(0).toEqualTypeOf<Dimension>();
+		expectTypeOf(sizeOf).returns.toBeNumber();
+	});
 });
