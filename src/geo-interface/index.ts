8a293a115fb16934e6a00b68a270895b73ec5a27
Kyle Barron <kyle@developmentseed.org>
2026-04-13 11:08:01 -0400

    Rename to `Interface`

--- src/geo-interface/index.ts
+++ src/geo-interface/index.ts
@@ -1,77 +1,77 @@
 export type Dimension = "XY" | "XYZ";
 
 export function sizeOf(dim: Dimension): number {
-  switch (dim) {
-    case "XY":
-      return 2;
-    case "XYZ":
-      return 3;
-  }
+	switch (dim) {
+		case "XY":
+			return 2;
+		case "XYZ":
+			return 3;
+	}
 }
 
-export interface CoordTrait {
-  dim(): Dimension;
-  x(): number;
-  y(): number;
-  nth(n: number): number;
+export interface CoordInterface {
+	dim(): Dimension;
+	x(): number;
+	y(): number;
+	nth(n: number): number;
 }
 
-export interface PointTrait {
-  readonly geometryType: "Point";
-  dim(): Dimension;
-  coord(): CoordTrait | null;
+export interface PointInterface {
+	readonly geometryType: "Point";
+	dim(): Dimension;
+	coord(): CoordInterface | null;
 }
 
-export interface LineStringTrait {
-  readonly geometryType: "LineString";
-  dim(): Dimension;
-  numCoords(): number;
-  coord(i: number): CoordTrait;
+export interface LineStringInterface {
+	readonly geometryType: "LineString";
+	dim(): Dimension;
+	numCoords(): number;
+	coord(i: number): CoordInterface;
 }
 
-export interface PolygonTrait {
-  readonly geometryType: "Polygon";
-  dim(): Dimension;
-  numInteriors(): number;
-  exterior(): LineStringTrait | null;
-  interior(i: number): LineStringTrait;
+export interface PolygonInterface {
+	readonly geometryType: "Polygon";
+	dim(): Dimension;
+	numInteriors(): number;
+	exterior(): LineStringInterface | null;
+	interior(i: number): LineStringInterface;
 }
 
-export interface MultiPointTrait {
-  readonly geometryType: "MultiPoint";
-  dim(): Dimension;
-  numPoints(): number;
-  point(i: number): PointTrait;
+export interface MultiPointInterface {
+	readonly geometryType: "MultiPoint";
+	dim(): Dimension;
+	numPoints(): number;
+	point(i: number): PointInterface;
 }
 
-export interface MultiLineStringTrait {
-  readonly geometryType: "MultiLineString";
-  dim(): Dimension;
-  numLineStrings(): number;
-  lineString(i: number): LineStringTrait;
+export interface MultiLineStringInterface {
+	readonly geometryType: "MultiLineString";
+	dim(): Dimension;
+	numLineStrings(): number;
+	lineString(i: number): LineStringInterface;
 }
 
-export interface MultiPolygonTrait {
-  readonly geometryType: "MultiPolygon";
-  dim(): Dimension;
-  numPolygons(): number;
-  polygon(i: number): PolygonTrait;
+export interface MultiPolygonInterface {
+	readonly geometryType: "MultiPolygon";
+	dim(): Dimension;
+	numPolygons(): number;
+	polygon(i: number): PolygonInterface;
 }
 
-export interface GeometryCollectionTrait {
-  readonly geometryType: "GeometryCollection";
-  dim(): Dimension;
-  numGeometries(): number;
-  geometry(i: number): GeometryTrait;
+export interface GeometryCollectionInterface {
+	readonly geometryType: "GeometryCollection";
+	dim(): Dimension;
+	numGeometries(): number;
+	geometry(i: number): GeometryInterface;
 }
 
-export type GeometryTrait =
-  | PointTrait
-  | LineStringTrait
-  | PolygonTrait
-  | MultiPointTrait
-  | MultiLineStringTrait
-  | MultiPolygonTrait
-  | GeometryCollectionTrait;
+export type GeometryInterface =
+	| PointInterface
+	| LineStringInterface
+	| PolygonInterface
+	| MultiPointInterface
+	| MultiLineStringInterface
+	| MultiPolygonInterface
+	| GeometryCollectionInterface;
 
 export * from "./iter.js";
--- tests/geo-interface/fixtures.ts
+++ tests/geo-interface/fixtures.ts
@@ -1,151 +1,151 @@
 import type {
-  CoordTrait,
-  Dimension,
-  GeometryCollectionTrait,
-  GeometryTrait,
-  LineStringTrait,
-  MultiLineStringTrait,
-  MultiPointTrait,
-  MultiPolygonTrait,
-  PointTrait,
-  PolygonTrait,
+	CoordInterface,
+	Dimension,
+	GeometryCollectionInterface,
+	GeometryInterface,
+	LineStringInterface,
+	MultiLineStringInterface,
+	MultiPointInterface,
+	MultiPolygonInterface,
+	PointInterface,
+	PolygonInterface,
 } from "../../src/geo-interface/index.js";
 
-export class RefCoord implements CoordTrait {
-  constructor(
-    private readonly _values: readonly number[],
-    private readonly _dim: Dimension,
-  ) {}
-  dim(): Dimension {
-    return this._dim;
-  }
-  x(): number {
-    return this._values[0];
-  }
-  y(): number {
-    return this._values[1];
-  }
-  nth(n: number): number {
-    return this._values[n];
-  }
+export class RefCoord implements CoordInterface {
+	constructor(
+		private readonly _values: readonly number[],
+		private readonly _dim: Dimension,
+	) {}
+	dim(): Dimension {
+		return this._dim;
+	}
+	x(): number {
+		return this._values[0];
+	}
+	y(): number {
+		return this._values[1];
+	}
+	nth(n: number): number {
+		return this._values[n];
+	}
 }
 
-export class RefPoint implements PointTrait {
-  readonly geometryType = "Point" as const;
-  constructor(
-    private readonly _values: readonly number[] | null,
-    private readonly _dim: Dimension,
-  ) {}
-  dim(): Dimension {
-    return this._dim;
-  }
-  coord(): CoordTrait | null {
-    return this._values === null ? null : new RefCoord(this._values, this._dim);
-  }
+export class RefPoint implements PointInterface {
+	readonly geometryType = "Point" as const;
+	constructor(
+		private readonly _values: readonly number[] | null,
+		private readonly _dim: Dimension,
+	) {}
+	dim(): Dimension {
+		return this._dim;
+	}
+	coord(): CoordInterface | null {
+		return this._values === null ? null : new RefCoord(this._values, this._dim);
+	}
 }
 
-export class RefLineString implements LineStringTrait {
-  readonly geometryType = "LineString" as const;
-  constructor(
-    private readonly _coords: readonly (readonly number[])[],
-    private readonly _dim: Dimension,
-  ) {}
-  dim(): Dimension {
-    return this._dim;
-  }
-  numCoords(): number {
-    return this._coords.length;
-  }
-  coord(i: number): CoordTrait {
-    return new RefCoord(this._coords[i], this._dim);
-  }
+export class RefLineString implements LineStringInterface {
+	readonly geometryType = "LineString" as const;
+	constructor(
+		private readonly _coords: readonly (readonly number[])[],
+		private readonly _dim: Dimension,
+	) {}
+	dim(): Dimension {
+		return this._dim;
+	}
+	numCoords(): number {
+		return this._coords.length;
+	}
+	coord(i: number): CoordInterface {
+		return new RefCoord(this._coords[i], this._dim);
+	}
 }
 
-export class RefPolygon implements PolygonTrait {
-  readonly geometryType = "Polygon" as const;
-  constructor(
-    private readonly _exterior: RefLineString | null,
-    private readonly _interiors: readonly RefLineString[],
-    private readonly _dim: Dimension,
-  ) {}
-  dim(): Dimension {
-    return this._dim;
-  }
-  exterior(): LineStringTrait | null {
-    return this._exterior;
-  }
-  numInteriors(): number {
-    return this._interiors.length;
-  }
-  interior(i: number): LineStringTrait {
-    return this._interiors[i];
-  }
+export class RefPolygon implements PolygonInterface {
+	readonly geometryType = "Polygon" as const;
+	constructor(
+		private readonly _exterior: RefLineString | null,
+		private readonly _interiors: readonly RefLineString[],
+		private readonly _dim: Dimension,
+	) {}
+	dim(): Dimension {
+		return this._dim;
+	}
+	exterior(): LineStringInterface | null {
+		return this._exterior;
+	}
+	numInteriors(): number {
+		return this._interiors.length;
+	}
+	interior(i: number): LineStringInterface {
+		return this._interiors[i];
+	}
 }
 
-export class RefMultiPoint implements MultiPointTrait {
-  readonly geometryType = "MultiPoint" as const;
-  constructor(
-    private readonly _points: readonly RefPoint[],
-    private readonly _dim: Dimension,
-  ) {}
-  dim(): Dimension {
-    return this._dim;
-  }
-  numPoints(): number {
-    return this._points.length;
-  }
-  point(i: number): PointTrait {
-    return this._points[i];
-  }
+export class RefMultiPoint implements MultiPointInterface {
+	readonly geometryType = "MultiPoint" as const;
+	constructor(
+		private readonly _points: readonly RefPoint[],
+		private readonly _dim: Dimension,
+	) {}
+	dim(): Dimension {
+		return this._dim;
+	}
+	numPoints(): number {
+		return this._points.length;
+	}
+	point(i: number): PointInterface {
+		return this._points[i];
+	}
 }
 
-export class RefMultiLineString implements MultiLineStringTrait {
-  readonly geometryType = "MultiLineString" as const;
-  constructor(
-    private readonly _lineStrings: readonly RefLineString[],
-    private readonly _dim: Dimension,
-  ) {}
-  dim(): Dimension {
-    return this._dim;
-  }
-  numLineStrings(): number {
-    return this._lineStrings.length;
-  }
-  lineString(i: number): LineStringTrait {
-    return this._lineStrings[i];
-  }
+export class RefMultiLineString implements MultiLineStringInterface {
+	readonly geometryType = "MultiLineString" as const;
+	constructor(
+		private readonly _lineStrings: readonly RefLineString[],
+		private readonly _dim: Dimension,
+	) {}
+	dim(): Dimension {
+		return this._dim;
+	}
+	numLineStrings(): number {
+		return this._lineStrings.length;
+	}
+	lineString(i: number): LineStringInterface {
+		return this._lineStrings[i];
+	}
 }
 
-export class RefMultiPolygon implements MultiPolygonTrait {
-  readonly geometryType = "MultiPolygon" as const;
-  constructor(
-    private readonly _polygons: readonly RefPolygon[],
-    private readonly _dim: Dimension,
-  ) {}
-  dim(): Dimension {
-    return this._dim;
-  }
-  numPolygons(): number {
-    return this._polygons.length;
-  }
-  polygon(i: number): PolygonTrait {
-    return this._polygons[i];
-  }
+export class RefMultiPolygon implements MultiPolygonInterface {
+	readonly geometryType = "MultiPolygon" as const;
+	constructor(
+		private readonly _polygons: readonly RefPolygon[],
+		private readonly _dim: Dimension,
+	) {}
+	dim(): Dimension {
+		return this._dim;
+	}
+	numPolygons(): number {
+		return this._polygons.length;
+	}
+	polygon(i: number): PolygonInterface {
+		return this._polygons[i];
+	}
 }
 
-export class RefGeometryCollection implements GeometryCollectionTrait {
-  readonly geometryType = "GeometryCollection" as const;
-  constructor(
-    private readonly _geometries: readonly GeometryTrait[],
-    private readonly _dim: Dimension,
-  ) {}
-  dim(): Dimension {
-    return this._dim;
-  }
-  numGeometries(): number {
-    return this._geometries.length;
-  }
-  geometry(i: number): GeometryTrait {
-    return this._geometries[i];
-  }
+export class RefGeometryCollection implements GeometryCollectionInterface {
+	readonly geometryType = "GeometryCollection" as const;
+	constructor(
+		private readonly _geometries: readonly GeometryInterface[],
+		private readonly _dim: Dimension,
+	) {}
+	dim(): Dimension {
+		return this._dim;
+	}
+	numGeometries(): number {
+		return this._geometries.length;
+	}
+	geometry(i: number): GeometryInterface {
+		return this._geometries[i];
+	}
 }
