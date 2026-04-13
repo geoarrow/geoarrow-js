b9c5502818231aca4ce824c13771eb2a58171c46
Kyle Barron <kyle@developmentseed.org>
2026-04-13 11:36:35 -0400

    fmt

--- src/worker/transferable.ts
+++ src/worker/transferable.ts
@@ -5,75 +5,75 @@ import { hardClone } from "./hard-clone";
  * Prepare a `Data` or `Vector` for a `postMessage` or `structuredClone`.
  */
 export function preparePostMessage<T extends DataType>(
-	input: Data<T>,
-	forceClone?: boolean,
+  input: Data<T>,
+  forceClone?: boolean,
 ): [Data<T>, ArrayBuffer[]];
 export function preparePostMessage<T extends DataType>(
-	input: Vector<T>,
-	forceClone?: boolean,
+  input: Vector<T>,
+  forceClone?: boolean,
 ): [Vector<T>, ArrayBuffer[]];
 
 export function preparePostMessage<T extends DataType>(
-	input: Data<T> | Vector<T>,
-	forceClone: boolean = false,
+  input: Data<T> | Vector<T>,
+  forceClone: boolean = false,
 ): [Data<T> | Vector<T>, ArrayBuffer[]] {
-	// Check if `input` is an arrow.Vector
-	if ("data" in input) {
-		const postMessageDatas: Data<T>[] = [];
-		const transferArrayBuffers: ArrayBuffer[] = [];
-		for (const data of input.data) {
-			const [postMessageData, arrayBuffers] = preparePostMessage(data);
-			postMessageDatas.push(postMessageData);
-			transferArrayBuffers.push(...arrayBuffers);
-		}
-		const vector = new Vector(postMessageDatas);
-		return [vector, transferArrayBuffers];
-	}
+  // Check if `input` is an arrow.Vector
+  if ("data" in input) {
+    const postMessageDatas: Data<T>[] = [];
+    const transferArrayBuffers: ArrayBuffer[] = [];
+    for (const data of input.data) {
+      const [postMessageData, arrayBuffers] = preparePostMessage(data);
+      postMessageDatas.push(postMessageData);
+      transferArrayBuffers.push(...arrayBuffers);
+    }
+    const vector = new Vector(postMessageDatas);
+    return [vector, transferArrayBuffers];
+  }
 
-	// Force clone into non-shared backing ArrayBuffers
-	// Note: this only clones if necessary, unless forceClone is `true`.
-	input = hardClone(input, forceClone);
+  // Force clone into non-shared backing ArrayBuffers
+  // Note: this only clones if necessary, unless forceClone is `true`.
+  input = hardClone(input, forceClone);
 
-	const transferArrayBuffers: ArrayBuffer[] = [];
+  const transferArrayBuffers: ArrayBuffer[] = [];
 
-	// Handle children
-	for (let childIdx = 0; childIdx < input.children.length; childIdx++) {
-		const childData = input.children[childIdx];
-		const [postMessageData, arrayBuffers] = preparePostMessage(childData);
-		input.children[childIdx] = postMessageData;
-		transferArrayBuffers.push(...arrayBuffers);
-	}
+  // Handle children
+  for (let childIdx = 0; childIdx < input.children.length; childIdx++) {
+    const childData = input.children[childIdx];
+    const [postMessageData, arrayBuffers] = preparePostMessage(childData);
+    input.children[childIdx] = postMessageData;
+    transferArrayBuffers.push(...arrayBuffers);
+  }
 
-	// Handle dictionary
-	if (input.dictionary !== undefined) {
-		const [postMessageVector, arrayBuffers] = preparePostMessage(
-			input.dictionary,
-		);
-		input.dictionary = postMessageVector;
-		transferArrayBuffers.push(...arrayBuffers);
-	}
+  // Handle dictionary
+  if (input.dictionary !== undefined) {
+    const [postMessageVector, arrayBuffers] = preparePostMessage(
+      input.dictionary,
+    );
+    input.dictionary = postMessageVector;
+    transferArrayBuffers.push(...arrayBuffers);
+  }
 
-	// Get references to the underlying buffers.
+  // Get references to the underlying buffers.
 
-	// We don't use a loop over these four to ensure accurate typing (well, typing
-	// doesn't seem to work on `DATA` and `TYPE`.)
-	if (input.buffers[BufferType.OFFSET] !== undefined) {
-		// @ts-expect-error TODO: check this
-		transferArrayBuffers.push(input.buffers[BufferType.OFFSET].buffer);
-	}
+  // We don't use a loop over these four to ensure accurate typing (well, typing
+  // doesn't seem to work on `DATA` and `TYPE`.)
+  if (input.buffers[BufferType.OFFSET] !== undefined) {
+    // @ts-expect-error TODO: check this
+    transferArrayBuffers.push(input.buffers[BufferType.OFFSET].buffer);
+  }
 
-	if (input.buffers[BufferType.DATA] !== undefined) {
-		// @ts-expect-error TODO: check this
-		transferArrayBuffers.push(input.buffers[BufferType.DATA].buffer);
-	}
-	if (input.buffers[BufferType.VALIDITY] !== undefined) {
-		// @ts-expect-error TODO: check this
-		transferArrayBuffers.push(input.buffers[BufferType.VALIDITY].buffer);
-	}
-	if (input.buffers[BufferType.TYPE] !== undefined) {
-		// @ts-expect-error TODO: check this
-		transferArrayBuffers.push(input.buffers[BufferType.TYPE].buffer);
-	}
+  if (input.buffers[BufferType.DATA] !== undefined) {
+    // @ts-expect-error TODO: check this
+    transferArrayBuffers.push(input.buffers[BufferType.DATA].buffer);
+  }
+  if (input.buffers[BufferType.VALIDITY] !== undefined) {
+    // @ts-expect-error TODO: check this
+    transferArrayBuffers.push(input.buffers[BufferType.VALIDITY].buffer);
+  }
+  if (input.buffers[BufferType.TYPE] !== undefined) {
+    // @ts-expect-error TODO: check this
+    transferArrayBuffers.push(input.buffers[BufferType.TYPE].buffer);
+  }
 
-	return [input, transferArrayBuffers];
+  return [input, transferArrayBuffers];
 }
